74121a263e53b50924967fd3a625a22bbea6c6fb
Marcelo W F <marcelowzorekfilho@gmail.com>
2024-02-16 15:43:31 -0300

    Add Test Folder

---
+++ OurDoorsTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OurDoorsTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
